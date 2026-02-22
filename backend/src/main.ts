7cde9c38d492d2f6c135e4ece1d5fdd0ac27ce35
gricardov <gricardov@gmail.com>
2026-02-22 18:07:10 -0500

    fix(backend): set CORS origin to true for dynamic origin reflection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,8 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
 
   const port = process.env.PORT || 4000;
